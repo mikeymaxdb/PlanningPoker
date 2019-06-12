85c7911533b3b617a017d16a9e99c55f92770ff5
mike <mikey.maxdb@gmail.com>
2019-06-11 21:23:46 -0700

    updated dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,40 @@
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-	entry: __dirname+"/src/apps/planning.js",
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader'
-			},
-			{
+    mode: 'production',
+    entry: './src/apps/planning.js',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
                 test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-		          fallback: 'style-loader',
-		          use: ['css-loader', 'sass-loader']
-		        })
-            }
-		]
-	},
-	output: {
-		filename: 'bundle.js',
-		path: __dirname+"/public/build"
-	},
-	plugins: [
-        new ExtractTextPlugin({
-		    filename:  "bundle.css",
-		    allChunks: true
-		  })
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            includePaths: [path.resolve(__dirname, 'src')],
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: __dirname+"/public/build"
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: 'bundle.css',
+        }),
     ]
 };
